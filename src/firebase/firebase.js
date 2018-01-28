aba5bd12d1e6f7037b8ddfd3aaf12e1b24540769
shtolfensie <31325217+shtolfensie@users.noreply.github.com>
2018-01-28 22:20:44 +0100

    Added Login functionality using react router

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+  import * as firebase from 'firebase';
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBL5ADb05I6Fuf5J5de0EJHmff45h3-gNk",
+    authDomain: "life-69419.firebaseapp.com",
+    databaseURL: "https://life-69419.firebaseio.com",
+    projectId: "life-69419",
+    storageBucket: "life-69419.appspot.com",
+    messagingSenderId: "840427893914"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+
+  const auth = firebase.auth();
+
+  export {
+    auth,
+  };
